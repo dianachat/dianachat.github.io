5c9d724ab52644c68eaa5bfa10c3b2b36a0bec64
dianachat.github.io <zuliganxx@hotmail.com>
2017-01-09 21:17:58 -0300

    added page

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+	<head>
+		<title>Dianachat</title>
+
+	  	<script type="text/javascript">
+			var headID = document.getElementsByTagName("head")[0];
+			var newCss = document.createElement('link');
+			newCss.rel = 'stylesheet';
+			newCss.type = 'text/css';
+			window._botUsername = '527544';
+			window._botName = 'Diana';
+			newCss.href = "/css/chatbost.css";
+			var newScript = document.createElement('script');
+			newScript.src = "http://rebot.me/assets/js/bot.js";
+			newScript.type = 'text/javascript';
+			headID.appendChild(newScript);
+			headID.appendChild(newCss);
+		</script>
+
+		<link rel="stylesheet" type="text/css" href="/css/page.css" />
+	</head>
+	<body>
+		<div id="diana"></div>
+		<div id="arrow"></div>
+		<div id="Container">
+				<div id="titulo"></div>
+				<div class="buble">
+					<p><u>Las Reglas Son Éstas:</u><br><br> *No puedes utilizar emoticones/emojis.<br> *Las faltas de ortografía no están permitidas. <br>*Las preguntas deben ser claras. <br>*No se pueden usar palabras en otro idioma. <br>*El uso de signos de pregunta (¿?), signos de exclamación (¡!), tilde/acento (´), mayúsculas o minúsculas en una frase no interfiere en el entendimiento de la oración, por lo que eres libre de utilizarlos o no.<br>
+Si alguna de éstas reglas se rompen, difícilmente se pueda llegar a tener una conversación coherente.<br><br>
+
+Siéntete libre de preguntar lo que quieras! Si la respuesta recibida no fue satisfactoria puedes volver a intentarlo al día siguiente y posiblemente obtengas una mejor respuesta.
+Constantemente Diana se encuentra actualizándose y aprendiendo cosas nuevas, su curiosidad por saberlo todo y aprender más del mundo y de ella misma no tiene límites.
+Si buscas una amiga, alguien con quien hablar o descubrir los misterios y secretos ocultos que se encuentran aquí, éste es el lugar indicado.</p>
+				</div>
+		</div>
+	</body>
+</html>
